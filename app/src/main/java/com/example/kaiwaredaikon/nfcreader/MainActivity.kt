ae1329cfb4f4a288ace2c12f2f71fbeab8d68795
kaiware daikon <kaiwaredaikon321@gmail.com>
2019-08-18 23:47:22 +0900

    newIntentを使用してIdを取得する方法

--- app/src/main/java/com/example/kaiwaredaikon/nfcreader/MainActivity.kt
+++ app/src/main/java/com/example/kaiwaredaikon/nfcreader/MainActivity.kt
@@ -11,6 +11,7 @@ import android.os.Handler
 import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.example.mylibrary.TestNfcReader
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.concurrent.schedule
@@ -18,76 +19,55 @@ import kotlin.concurrent.schedule
 class MainActivity : AppCompatActivity() {
 
     private val handler = Handler()
-
+    private var testNfcReader = TestNfcReader()
     private var status = 0
-    private val nfcAdapter by lazy { NfcAdapter.getDefaultAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setText(STATUS_WAIT)
-      checkNfcStatus()
-        // NfcAdapter の検出開始を実行する。
+        setTextView(STATUS_WAIT)
+        testNfcReader.initNfcAdapter( this )
     }
 
     override fun onResume() {
-
-        Log.d("NFC", "onResume ");
         super.onResume()
+        testNfcReader.startDetection( this, this )
+    }
 
-        // PendingIntent 起動時に実行される intent の生成と PendingIntent のインスタンス化をする。
-        val intent = Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        val feliCaPendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-
-        // NfcAdapter.ACTION_NDEF_DISCOVERED をアクション名とするフィルタを生成する。
-        val filter = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED).apply { this.addDataType("*/*") }
-        nfcAdapter.enableForegroundDispatch(this, feliCaPendingIntent, arrayOf(filter), arrayOf(NFC_TYPES))
+    override fun onPause() {
+        super.onPause()
+        testNfcReader.endDetection( this )
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        Log.d("NFC", "感知完治感知")
 
         if (status == STATUS_WAIT) {
-            Log.d("NFC", "変更変更")
-            setText(STATUS_SUCCESS)
+
+            testNfcReader.getUid(intent)
+
+            setTextView(STATUS_SUCCESS)
 
             Timer().schedule(3000, 3000, {
-                Log.d("NFC", "タイマータイマー")
                 handler.post {
-                    setText(STATUS_WAIT)
+                    setTextView(STATUS_WAIT)
                 }
-                Log.d("NFC", "タイマータイマー2")
                 this.cancel()
             })
         }
     }
 
-    private fun setText(sts: Int) {
+    private fun setTextView(sts: Int) {
 
         status = sts
 
-        Log.d("NFC", "sts$sts")
-        when (sts) {
-            STATUS_WAIT -> textView.text = "Capiカードをかざしてください。"
-            STATUS_SUCCESS -> textView.text = "カードを読み込みました"
-            STATUS_INVALID -> textView.text = "NFCが無効になっています。"
-        }
-    }
+        Log.d("MainActivity", "sts$sts")
 
-    private fun checkNfcStatus() {
-        val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if (!nfcAdapter.isEnabled) {
-            AlertDialog.Builder(this).also {
-                it.setMessage("NFCが無効になっています。" + "NFC設定画面を開きますか？")
-                it.setPositiveButton("はい") { _, _ ->
-                    startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
-                }
-                it.setNegativeButton("いいえ") { _, _ ->
-                    setText(STATUS_INVALID)
-                }
-            }.show()
+        when (sts) {
+            STATUS_WAIT -> textView.text = "カードをかざしてください。"
+            STATUS_SUCCESS -> textView.text = "カードを読み込みました"
+            STATUS_INVALID -> textView.text = "無効になっています。"
         }
     }
 
@@ -95,6 +75,5 @@ class MainActivity : AppCompatActivity() {
         const val STATUS_WAIT = 1
         const val STATUS_SUCCESS = 2
         const val STATUS_INVALID = 3
-        val NFC_TYPES = arrayOf(NfcF::class.java.name)
     }
 }
--- mylibrary/src/main/java/com/example/mylibrary/TestNfcReader.java
+++ mylibrary/src/main/java/com/example/mylibrary/TestNfcReader.java
@@ -1,10 +1,40 @@
 package com.example.mylibrary;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
 import android.util.Log;
 
+import java.util.Arrays;
 
-public class TestNfcReader {
-    public void startDetection(){
-        Log.d("Nfc","startDetection");
+public final class TestNfcReader {
+
+    private NfcAdapter nfcAdapter;
+
+    public void initNfcAdapter( Context context ){
+        nfcAdapter = NfcAdapter.getDefaultAdapter( context );
+
+        if( !nfcAdapter.isEnabled() ){
+        }
+    }
+
+    public void startDetection(Context context, Activity activity ){
+        Intent intent = new Intent(context, context.getClass() );
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+        nfcAdapter.enableForegroundDispatch( activity, pendingIntent, null, null);
+    }
+
+    public void endDetection(Activity activity){
+        nfcAdapter.disableForegroundDispatch(activity);
+    }
+
+    public byte[] getUid(Intent intent ){
+        byte[] uid = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+        Log.d( "TestNfcReader", "uid = " + Arrays.toString(uid));
+        return uid;
     }
 }
